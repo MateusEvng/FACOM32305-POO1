e5f48e7a908a131ae967c5783ee82946ea79335a
Mateus Evangelista <105022974+MateusEvng@users.noreply.github.com>
2024-01-18 20:32:27 -0300

    UPLOAD | 1 | Inicio da atividade
    Feito upload da atividade 00
    
    Resolvidos os exercícios a e b
    
    Pendente: continuar o desenvolvimento da letra c e finalizar a atividade como um todo

---
+++ 00 - Comandos_Basicos/src/c/Main.java
@@ -0,0 +1,13 @@
+package c;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        Triangulo triangulo[] = new Triangulo[3]; //depois alterar para 10
+
+    }
+}
---
+++ 00 - Comandos_Basicos/src/c/Triangulo.java
@@ -0,0 +1,35 @@
+package c;
+
+public class Triangulo {
+    
+    public double Lado1, Lado2, Lado3;
+
+    //getters e setters
+    public double getLado1() {
+        return Lado1;
+    }
+    public double getLado2() {
+        return Lado2;
+    }
+    public double getLado3() {
+        return Lado3;
+    }
+
+    public void setLado1(double lado1) {
+        Lado1 = lado1;
+    }
+    public void setLado2(double lado2) {
+        Lado2 = lado2;
+    }
+    public void setLado3(double lado3) {
+        Lado3 = lado3;
+    }
+
+    //perimetro
+    double calculaPerimetro(){
+        return this.Lado1 + this.Lado2 + this.Lado3;
+        //return getLado1() + getLado2() + getLado3();
+    }
+
+
+}
